79d92c441cf6deb1f7a9c0afcbdc29c855212a7b
perlaavila <perlaavila348@gmail.com>
2018-05-29 22:42:05 -0700

    Added views to create/edit blog post inside the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, comment
 
 admin.site.register(Post)
+admin.site.register(comment)
+
 # Register your models here.
